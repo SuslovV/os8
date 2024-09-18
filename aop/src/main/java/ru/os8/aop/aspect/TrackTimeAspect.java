fd3087ed0e01175e65779c00c80a9e0e52dd9e04
vadim <suslovv@yandex.ru>
2024-09-19 02:17:47 +0300

    Exceptions aspect corrected

--- aop/src/main/java/ru/os8/aop/aspect/TrackTimeAspect.java
+++ aop/src/main/java/ru/os8/aop/aspect/TrackTimeAspect.java
@@ -6,7 +6,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
 import ru.os8.aop.service.PerfomanceStatisticsService;
 
@@ -48,11 +47,15 @@ public class TrackTimeAspect {
 
         Object result = proceedingJoinPoint.proceed();
 
-        if (!trackAsync) {
-            savePerfomanceStatistics(startTime, className, methodName, result);
-        } else {
-            CompletableFuture future = (CompletableFuture) result;
-            future.thenAccept(o -> savePerfomanceStatistics(startTime, className, methodName, result));
+        try {
+            if (!trackAsync) {
+                savePerfomanceStatistics(startTime, className, methodName, result);
+            } else {
+                CompletableFuture future = (CompletableFuture) result;
+                future.thenAccept(o -> savePerfomanceStatistics(startTime, className, methodName, result));
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
         }
 
         return result;
