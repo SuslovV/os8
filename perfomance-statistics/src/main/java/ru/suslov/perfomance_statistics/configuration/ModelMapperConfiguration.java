6057787a1dbf524d8b29f0d9e557898695481037
vadim <suslovv@yandex.ru>
2024-10-09 03:28:02 +0300

    HttpRequestLog map

--- perfomance-statistics/src/main/java/ru/suslov/perfomance_statistics/configuration/ModelMapperConfiguration.java
+++ perfomance-statistics/src/main/java/ru/suslov/perfomance_statistics/configuration/ModelMapperConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ModelMapperConfiguration2 {
+public class ModelMapperConfiguration {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
